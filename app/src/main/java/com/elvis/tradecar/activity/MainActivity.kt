cc08c8a95b533e3f30d5dccb7b25deb078c63a6f
Elvis <hy412145@163.com>
2017-06-29 08:05:44 +0800

    add Kotterknife

--- app/src/main/java/com/elvis/tradecar/activity/MainActivity.kt
+++ app/src/main/java/com/elvis/tradecar/activity/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
+import butterknife.bindView
 import com.elvis.tradecar.R
 
 class MainActivity : AppCompatActivity() {
 
-    private var mTextMessage: TextView? = null
+    val mTextMessage: TextView by bindView(R.id.message);
+    val mNavigation: BottomNavigationView by bindView(R.id.navigation);
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -31,10 +33,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        mTextMessage = findViewById(R.id.message) as TextView
-        val navigation = findViewById(R.id.navigation) as BottomNavigationView
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        mNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
-
 }
